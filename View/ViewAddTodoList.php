3369f8f2da65ca566f155cfda230b6553861eabf
Irfan Yahya Abdillah <irfanyahyaa@gmail.com>
2024-10-08 17:01:48 +0700

    feat: add viewRemoveTodoList function at ViewRemoveTodoList.php

--- View/ViewAddTodoList.php
+++ View/ViewAddTodoList.php
@@ -9,6 +9,7 @@ function viewAddTodoList(): void
     echo "Add Todo" . PHP_EOL;
 
     $todo = input("Todo (x to exit)");
+
     if ($todo == "x") {
         echo "Cancel adding todo" . PHP_EOL;
     } else {
--- View/ViewRemoveTodoList.php
+++ View/ViewRemoveTodoList.php
@@ -1,6 +1,23 @@
 <?php
 
-function viewRemoveTodoList()
-{
+require_once "Helper/Input.php";
+require_once "BusinessLogic/RemoveTodoList.php";
 
+function viewRemoveTodoList(): void
+{
+    echo "Remove Todo" . PHP_EOL;
+
+    $input = input("Number (x to exit): ");
+
+    if ($input == "x") {
+        echo "Cancel removing todo" . PHP_EOL;
+    } else {
+        $success = removeTodoList((int) $input);
+
+        if ($success) {
+            echo "Remove todo number $input success" . PHP_EOL;
+        } else {
+            echo "Remove todo number $input failed" . PHP_EOL;
+        }
+    }
 }
