a2461d22e89b398828bc756979c3f383cbd83f28
Irfan Yahya Abdillah <irfanyahyaa@gmail.com>
2024-10-08 13:32:19 +0700

    feat: add removeTodoList function at RemoveTodoList.php

--- BusinessLogic/RemoveTodoList.php
+++ BusinessLogic/RemoveTodoList.php
@@ -3,7 +3,19 @@
 /**
  * remove todo from the list
  */
-function removeTodoList()
+function removeTodoList(int $key): bool
 {
+    global $todoList;
 
+    if ($key > sizeof($todoList)) {
+        return false;
+    }
+
+    for ($i = $key; $i < sizeof($todoList); $i++) {
+        $todoList[$i] = $todoList[$i + 1];
+    }
+
+    unset($todoList[sizeof($todoList)]);
+
+    return true;
 }
