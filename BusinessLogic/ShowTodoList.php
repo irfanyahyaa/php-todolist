a6662201e152bdb4a1370b87f331e474550a0bac
Irfan Yahya Abdillah <irfanyahyaa@gmail.com>
2024-10-08 10:49:42 +0700

    feat: add showTodoList function at ShowTodoList.php

--- BusinessLogic/ShowTodoList.php
+++ BusinessLogic/ShowTodoList.php
@@ -5,5 +5,11 @@
  */
 function showTodoList()
 {
+    global $todoList;
 
+    echo "Todo List" . PHP_EOL;
+
+    foreach ($todoList as $key => $value) {
+        echo "$key. $value" . PHP_EOL;
+    }
 }
