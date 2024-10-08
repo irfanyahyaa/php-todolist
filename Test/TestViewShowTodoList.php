45261b54f6084b7bff16830a8ddd07092a3c40a8
Irfan Yahya Abdillah <irfanyahyaa@gmail.com>
2024-10-08 16:43:00 +0700

    feat: add viewAddTodoList function at ViewAddTodoList.php

---
+++ Test/TestViewShowTodoList.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "Model/TodoList.php";
+require_once "View/ViewShowTodoList.php";
+
+$todoList = [
+  1 => "Learn PHP Fundamental",
+  2 => "Learn PHP OOP",
+  3 => "Learn PHP Database",
+  4 => "Learn PHP Web",
+  5 => "Learn PHP Framework",
+];
+
+viewShowTodoList();
--- View/ViewAddTodoList.php
+++ View/ViewAddTodoList.php
@@ -1,6 +1,17 @@
 <?php
 
-function viewAddTodoList()
-{
+require_once "Model/TodoList.php";
+require_once "BusinessLogic/AddTodoList.php";
+require_once "Helper/Input.php";
 
+function viewAddTodoList(): void
+{
+    echo "Add Todo" . PHP_EOL;
+
+    $todo = input("Todo (x to exit)");
+    if ($todo == "x") {
+        echo "Cancel adding todo" . PHP_EOL;
+    } else {
+        addTodoList($todo);
+    }
 }
