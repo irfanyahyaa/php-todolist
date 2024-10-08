b9d8c262ce5ee793c901e59b67140ac892ccb267
Irfan Yahya Abdillah <irfanyahyaa@gmail.com>
2024-10-08 16:35:31 +0700

    feat: add viewShowTodoList function at ViewShowTodoList.php

--- View/ViewShowTodoList.php
+++ View/ViewShowTodoList.php
@@ -1,6 +1,36 @@
 <?php
 
-function viewShowTodoList()
-{
+require_once "Model/TodoList.php";
+require_once "BusinessLogic/ShowTodoList.php";
+require_once "View/ViewAddTodoList.php";
+require_once "View/ViewRemoveTodoList.php";
+require_once "Helper/Input.php";
 
+function viewShowTodoList(): void
+{
+    while (true) {
+        showTodoList();
+
+        echo "Menu" . PHP_EOL;
+        echo "1. Add Todo" . PHP_EOL;
+        echo "2. Remove Todo" . PHP_EOL;
+        echo "x. Exit" . PHP_EOL;
+
+        $input = input("Select menu");
+
+        switch ($input) {
+            case "1":
+                viewAddTodoList();
+                break;
+            case "2":
+                viewRemoveTodoList();
+                break;
+            case "x":
+                echo "Bye" . PHP_EOL;
+                return;
+            default:
+                echo "Wrong input" . PHP_EOL;
+                break;
+        }
+    }
 }
